be695bcccb0fc68aa954fd700944fb20bc5b598c
Hubert Tatar <h.tatar@adform.com>
2022-03-24 15:54:58 +0100

    refactor

--- src/main/scala/io/huta/joins/dsl/StreamToStreamLeft.scala
+++ src/main/scala/io/huta/joins/dsl/StreamToStreamLeft.scala
@@ -4,7 +4,7 @@ import io.huta.common._
 import org.apache.kafka.clients.admin.{AdminClient, NewTopic}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.kafka.common.serialization.{IntegerSerializer, Serde, Serdes}
-import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.{KafkaStreams, Topology}
 import org.apache.kafka.streams.kstream.JoinWindows
 import org.apache.kafka.streams.scala.StreamsBuilder
 
@@ -16,31 +16,15 @@ import scala.jdk.CollectionConverters._
 object StreamToStreamLeft extends AdminConnectionProps with ProducerDefault with SetupTopic with Logging {
 
   def main(args: Array[String]): Unit = {
-//    setup(kfkProps())
-//    new Thread(producer1(producerProperties())).start()
-//    new Thread(producer2(producerProperties())).start()
+    //    setup(kfkProps())
+    //    new Thread(producer1(producerProperties())).start()
+    //    new Thread(producer2(producerProperties())).start()
     join()
   }
 
   def join(): Unit = {
-    //implcits for Consumed.with for builder.stream
-    import org.apache.kafka.streams.scala.ImplicitConversions._
-    import org.apache.kafka.streams.scala.serialization.Serdes._
-    implicit def stream1Serde: Serde[Stream1Data] = Serdes.serdeFrom(new JsonSerializer[Stream1Data], new JsonDeserializer[Stream1Data])
-    implicit def stream2Serde: Serde[Stream2Data] = Serdes.serdeFrom(new JsonSerializer[Stream2Data], new JsonDeserializer[Stream2Data])
-    implicit def streamJoined: Serde[StreamJoined] = Serdes.serdeFrom(new JsonSerializer[StreamJoined], new JsonDeserializer[StreamJoined])
-
     val props = kfkProps()
-    val builder = new StreamsBuilder
-    val stream1 = builder.stream[Int, Stream1Data]("join_topic_3_part")
-    val stream2 = builder.stream[Int, Stream2Data]("join_topic_6_part")
-
-    val streamsJoined = stream1.leftJoin(stream2)(
-      (s1data, s2data) => StreamJoined(s1data.key, s2data.key, s1data.status, s2data.status),
-      JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofSeconds(30))
-    ).to("join_output_diff_part")
-
-    val streams = new KafkaStreams(builder.build(), props)
+    val streams = new KafkaStreams(topology(), props)
     streams.cleanUp()
     streams.start()
 
@@ -49,12 +33,32 @@ object StreamToStreamLeft extends AdminConnectionProps with ProducerDefault with
     }
   }
 
+  def topology(): Topology = {
+    //implcits for Consumed.with for builder.stream
+    import org.apache.kafka.streams.scala.ImplicitConversions._
+    import org.apache.kafka.streams.scala.serialization.Serdes._
+
+    implicit def stream1Serde: Serde[Stream1Data] = Serdes.serdeFrom(new JsonSerializer[Stream1Data], new JsonDeserializer[Stream1Data])
+    implicit def stream2Serde: Serde[Stream2Data] = Serdes.serdeFrom(new JsonSerializer[Stream2Data], new JsonDeserializer[Stream2Data])
+    implicit def streamJoined: Serde[StreamJoined] = Serdes.serdeFrom(new JsonSerializer[StreamJoined], new JsonDeserializer[StreamJoined])
+
+    val builder = new StreamsBuilder
+    val stream1 = builder.stream[Int, Stream1Data](LeftJoinTopics.input1)
+    val stream2 = builder.stream[Int, Stream2Data](LeftJoinTopics.input2)
+
+    stream1.leftJoin(stream2)(
+      (s1data, s2data) => StreamJoined(s1data.key, s2data.key, s1data.status, s2data.status),
+      JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofSeconds(30))
+    ).to(LeftJoinTopics.output)
+    builder.build()
+  }
+
   def producer2(props: Properties): Runnable = {
     () => {
       val producer = new KafkaProducer(props, new IntegerSerializer, new JsonSerializer[Stream2Data])
 
       for (i <- 1 to 1_000_000 by 2) {
-        producer.send(new ProducerRecord("join_topic_6_part", i, Stream2Data(i, "StatusA")))
+        producer.send(new ProducerRecord(LeftJoinTopics.input2, i, Stream2Data(i, "StatusA")))
       }
       log.info("producer 2 finished")
       producer.flush()
@@ -67,7 +71,7 @@ object StreamToStreamLeft extends AdminConnectionProps with ProducerDefault with
       val producer = new KafkaProducer(props, new IntegerSerializer, new JsonSerializer[Stream1Data])
 
       for (i <- 1 to 1_000_000 by 3) {
-        producer.send(new ProducerRecord("join_topic_3_part", i, Stream1Data(i, "StatusB")))
+        producer.send(new ProducerRecord(LeftJoinTopics.input1, i, Stream1Data(i, "StatusB")))
       }
       log.info("producer 1 finished")
       producer.flush()
@@ -76,6 +80,12 @@ object StreamToStreamLeft extends AdminConnectionProps with ProducerDefault with
   }
 
   def setup(props: Properties): Unit = {
-    setupTopics(props, List("join_topic_3_part", "join_topic_6_part", "join_output_diff_part"))
+    setupTopics(props, List(LeftJoinTopics.input1, LeftJoinTopics.input2, LeftJoinTopics.output))
+  }
+
+  object LeftJoinTopics {
+    val input1 = "join_topic_1_part"
+    val input2 = "join_topic_2_part"
+    val output = "join_output_diff_part"
   }
 }
